be29dfba2b09d83cfd97bc8e8461768f86f271e9
TuanVu0929 <tranngoc.haidang0402@gmail.com>
2023-05-11 09:57:47 +0700

    file moi

--- CaseStudy2/src/ioFile/IOFile.java
+++
@@ -1,9 +0,0 @@
-//package ioFile;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public interface IOFile<E> {
-//    void writer(List<E> e, String path);
-//    ArrayList<E> read(String path);
-//}
